ac2976dccb229f78f13beae8829577cba4737d09
Bergan Oudshoorn <Bergan00@gmail.com>
2021-04-23 15:24:42 -0400

    working commands

--- bot.py
+++ bot.py
@@ -1,18 +1,54 @@
 import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 import os
 
-client = discord.Client()
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
-@client.event
+# client = discord.Client()
+
+# @client.event
+# async def on_ready():
+#   print('We have logged in as {0.user}'.format(client))
+
+# @client.event
+# async def on_message(message):
+#     if message.content.startswith('!set'):
+#       embedVar = discord.Embed(title='**Game**', description="Desc", color=discord.Colour.random())
+#       embedVar.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
+#       # embedVar.add_field(name="Field1", value="hi", inline=False)
+#       # embedVar.add_field(name="Field2", value="hi2", inline=False)
+#       await message.channel.send(embed=embedVar)
+
+# @client.event
+# async def on_member_join(member):
+#     await member.create_dm()
+#     await member.dm_channel.send(
+#         f'Hi {member.name}, welcome to my Discord server!'
+#     )
+
+
+# client.run(os.environ['TOKEN'])
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print('We have logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-      return
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the Discord server!'
+    )
 
-  if message.content.startswith('$hello'):
-      await message.channel.send('Hello!')
+@bot.command(name='set')
+async def set(ctx, game, time):
+  embedVar = discord.Embed(title=game, description=time, color=discord.Colour.random())
+  embedVar.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+  embedVar.add_field(name="Who's Joining", value="hi", inline=False)
+  # embedVar.add_field(name="Field2", value="hi2", inline=False)
+  await ctx.send(embed=embedVar)
 
-client.run(os.environ['TOKEN'])
+bot.run(TOKEN)
